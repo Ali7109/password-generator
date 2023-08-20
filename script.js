be5312b504f4a92cb716b5290dafde1f14548bef
Ali Amin <aliamin745@gmail.com>
2023-08-20 16:00:52 +0300

    Added entropy based password prediction check

--- script.js
+++ script.js
@@ -1,86 +1,163 @@
-const lengthSlider = document.querySelector(".pass-length input")
+// Get references to the relevant HTML elements
+const lengthSlider = document.querySelector(".pass-length input");
+const options = document.querySelectorAll(".option input");
+const generateBtn = document.querySelector(".generate-btn");
 
-options = document.querySelectorAll(".option input")
-generateBtn = document.querySelector(".generate-btn")
-
+// Define character sets for different types of characters
 const characters = {
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numbers: "0123456789",
-    symbols: "~`!@#$%^&*()_-+={[}]|::;\"'<,>.?"
+	lowercase: "abcdefghijklmnopqrstuvwxyz",
+	uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	numbers: "0123456789",
+	symbols: "~`!@#$%^&*()_-+={[}]|::;\"'<,>.?",
+};
+
+//Password strength complexity algorithm
+// Calculate password strength based on various factors
+function calculatePasswordStrength(password) {
+	const minLength = 8; // Minimum required password length
+	const minVariety = 3; // Minimum character variety (lowercase, uppercase, digits, symbols)
+	const minUnpredictability = 0.7; // Minimum unpredictability ratio
+
+	const lengthFactor = Math.min(1, password.length / minLength);
+	const varietyFactor = Math.min(
+		1,
+		calculateCharacterVariety(password) / minVariety
+	);
+	const unpredictabilityFactor =
+		calculateUnpredictability(password) / minUnpredictability;
+
+	const overallStrength =
+		(lengthFactor + varietyFactor + unpredictabilityFactor) / 3;
+
+	return overallStrength * 100;
 }
 
-generatePassword = () => {
-    let staticPass = "",
-        randomPassword = "",
-        passLength = lengthSlider.value,
-        spaces = false,
-        duplicates = true,
-        choices = 0
-    options.forEach(option => {
-        if (option.checked) {
-            staticPass += characters[option.id]
-            choices++;
-            if (option.id === "ex-dup") {
-                duplicates = false
-            }
-            if (option.id === "inc-spaces") {
-                spaces = true
-            }
-        }
-    });
-    if (spaces) {
-        staticPass.concat(staticPass.substring(0, 1), " ", staticPass.substring(3, staticPass.length))
-    }
-    for (let i = 0; i < passLength; i++) {
-        let char = staticPass[Math.floor(Math.random() * staticPass.length)]
-        if (!duplicates) {
-            if (!randomPassword.includes(char)) {
-                randomPassword += char
-            } else {
-                i--
-            }
-        } else {
-            randomPassword += char
-        }
-    }
+// Calculate the variety of characters used in the password
+function calculateCharacterVariety(password) {
+	const categories = {
+		lowercase: /[a-z]/,
+		uppercase: /[A-Z]/,
+		digits: /\d/,
+		symbols: /[!@#$%^&*()_\-+={[}\]|:;'"<,>.?~`]/,
+	};
 
-    let lengthStrength = passLength >= 15
-    let variance = choices >= 4
-
-    if (!lengthStrength && !variance) {
-        document.querySelector(".pass-indicator").style.backgroundColor = "red"
-        document.querySelector(".pass-indicator span").innerHTML = "Very Weak"
-        document.querySelector(".pass-indicator").style.width = "25%"
-    } else if (lengthStrength && variance) {
-        document.querySelector(".pass-indicator").style.backgroundColor = "green"
-        document.querySelector(".pass-indicator span").innerHTML = "Strong"
-        document.querySelector(".pass-indicator").style.width = "100%"
-    } else if (lengthStrength && !variance) {
-        document.querySelector(".pass-indicator").style.backgroundColor = "yellow"
-        document.querySelector(".pass-indicator span").innerHTML = "Good"
-        document.querySelector(".pass-indicator").style.width = "75%"
-    } else {
-        document.querySelector(".pass-indicator").style.backgroundColor = "orange"
-        document.querySelector(".pass-indicator span").innerHTML = "Weak"
-        document.querySelector(".pass-indicator").style.width = "50%"
-    }
-
-
-    const output = document.querySelector(".input-box input")
-    output.value = randomPassword
+	let variety = 0;
+	for (const category in categories) {
+		if (categories[category].test(password)) {
+			variety++;
+		}
+	}
+	return variety;
 }
+
+// Calculate the unpredictability ratio of the password
+function calculateUnpredictability(password) {
+	const entropy = calculateEntropy(password);
+	const maxEntropy = Math.log2(
+		Math.pow(characters.lowercase.length, password.length)
+	);
+
+	return entropy / maxEntropy;
+}
+
+// Calculate the entropy of the password
+function calculateEntropy(password) {
+	const uniqueCharacters = new Set(password);
+	const characterCount = password.length;
+	const uniqueCount = uniqueCharacters.size;
+
+	return Math.log2(Math.pow(uniqueCount, characterCount));
+}
+
+// Function to generate a password based on user options
+const generatePassword = () => {
+	// Initialize variables
+	let staticPass = "";
+	let randomPassword = "";
+	let passLength = lengthSlider.value;
+	let spaces = false;
+	let duplicates = true;
+	let choices = 0;
+
+	// Iterate through the options to build the character set
+	options.forEach((option) => {
+		if (option.checked) {
+			staticPass += characters[option.id];
+			choices++;
+			if (option.id === "ex-dup") {
+				duplicates = false;
+			}
+			if (option.id === "inc-spaces") {
+				spaces = true;
+			}
+		}
+	});
+
+	// Insert spaces if specified by the user
+	if (spaces) {
+		staticPass =
+			staticPass.substring(0, 1) +
+			" " +
+			staticPass.substring(3, staticPass.length);
+	}
+
+	// Generate the random password
+	for (let i = 0; i < passLength; i++) {
+		let char = staticPass[Math.floor(Math.random() * staticPass.length)];
+		if (!duplicates) {
+			if (!randomPassword.includes(char)) {
+				randomPassword += char;
+			} else {
+				i--;
+			}
+		} else {
+			randomPassword += char;
+		}
+	}
+
+	// Determine password strength and update the indicator
+	const indicator = document.querySelector(".pass-indicator");
+
+	let complexity = calculatePasswordStrength(randomPassword);
+	let indicatorVisualCol = indicator.querySelector("span");
+	let indicatorStyle = indicator.style;
+
+	indicatorStyle.width = `${complexity}%`;
+	if (complexity > 90) {
+		indicatorVisualCol.innerHTML = "Very Strong";
+		indicatorStyle.backgroundColor = "green";
+	} else if (complexity > 70) {
+		indicatorVisualCol.innerHTML = "Strong";
+		indicatorStyle.backgroundColor = "orange";
+	} else if (complexity > 50) {
+		indicatorVisualCol.innerHTML = "Moderate";
+		indicatorStyle.backgroundColor = "yellow";
+	} else {
+		indicatorVisualCol.innerHTML = "Weak";
+		indicatorStyle.backgroundColor = "red";
+	}
+
+	// Display the generated password in the output field
+	const output = document.querySelector(".input-box input");
+	output.value = randomPassword;
+};
+
+// Function to update the slider label based on its value
 const updateSlider = () => {
-    const len = document.querySelector(".pass-length .details span")
-    len.innerHTML = lengthSlider.value;
-}
-const copy2clip = () => {
-    const output = document.querySelector(".input-box input")
-    navigator.clipboard.writeText(output.value);
-    alert("Password copied to clipboard")
-}
+	const len = document.querySelector(".pass-length .details span");
+	len.innerHTML = lengthSlider.value;
+};
 
-updateSlider()
+// Function to copy the generated password to the clipboard
+const copyToClipboard = () => {
+	const output = document.querySelector(".input-box input");
+	navigator.clipboard.writeText(output.value);
+	alert("Password copied to clipboard");
+};
 
-lengthSlider.addEventListener("input", updateSlider)
-generateBtn.addEventListener("click", generatePassword)
+// Initialize the slider label
+updateSlider();
+
+// Event listeners for slider input and generate button click
+lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword);
